d7a936115da662c3bd28b5306fdf76fae59c3671
Will <gogobebe2@gmail.com>
2016-08-14 22:15:23 +1200

    Update loanrepayment.php

--- loanrepayment.php
+++ loanrepayment.php
@@ -1,29 +1,37 @@
 <?php
+
 /*
-Template Name: LoanRepayment
+
+Template Name: Loanrepayment
+
 */
+	get_header();
+?>
 
-get_header(); ?>
 
-	<div id="primary" class="content-area col-md-9">
-	    <?php include('html/loanrepayment.html'); ?>
-		<main id="main" class="post-wrap" role="main">
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
-
+				<?php include('html/loanrepayment.html'); ?>
+				
 				<?php get_template_part( 'content', 'page' ); ?>
 
 				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
+
+					if ( comments_open() || '0' != get_comments_number() ) :
+
 						comments_template();
+
 					endif;
 				?>
 
 			<?php endwhile; // end of the loop. ?>
 
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
+
